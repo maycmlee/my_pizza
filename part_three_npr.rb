8871060e47758706bdc17de2d579017c74a485d8
dancinghoneybees <opheliahl@hotmail.com>
2015-12-28 13:29:21 -0500

    add get audio urls method and refine query search

--- part_three_npr.rb
+++ part_three_npr.rb
@@ -17,13 +17,25 @@ class NPRQuery
     @search_term = search_term
   end
 
+  def self.get_pizza_audios
+    pizza = NPRQuery.new("pizza")
+    results = pizza.query
+    pizza.get_audio_urls(results)
+  end
+
   def query
     #search for audio based on search_term
     base_url = "http://api.npr.org/"
-    query_url = "#{base_url}query?id=1053&requiredAssets=audio&searchTerm=#{self.search_term}&dateType=story&output=JSON&apiKey=#{API_KEY}"
+    query_url = "#{base_url}query?&requiredAssets=audio&searchTerm=#{self.search_term}&dateType=story&searchType=mainText&output=JSON&apiKey=#{API_KEY}"
     result = open(query_url).read
     JSON.parse(result)
   end
+
+  def get_audio_urls(json_data)
+    json_data["list"]["story"].map do |data|
+      data["audio"][0]["format"]["mp3"].first["$text"]
+    end
+  end
 end
 
-NPRQuery.new("pizza").query
+NPRQuery.get_pizza_audios
